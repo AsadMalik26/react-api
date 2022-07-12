898a02489a2d53edf609791f1ff0311d1ad9256d
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2022-07-12 18:19:41 +0500

    Creating Top Menu

---
+++ src/components/TopMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TopMenu = () => {
+    return ( <div>
+        <ul>
+            <li><a href="/"> Home</a></li>
+            <li><a href="/"> About Us</a></li>
+            <li><a href="/"> Contact us</a></li>
+        </ul>
+    </div> );
+}
+ 
+export default TopMenu;
