a6d0047b25bd874075b5c00eab2a80d3e4ce65a3
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2022-07-12 20:49:43 +0500

    Installed Material UI

--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -1,3 +1,4 @@
+import { Button } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -7,6 +8,8 @@ const TopMenu = () => {
             <li style={{display:"inline",paddingRight:"50px"}}><Link to="/"> Home</Link></li>
             <li style={{display:"inline",paddingRight:"50px"}}><Link to="/product"> Product</Link></li>
             <li style={{display:"inline",paddingRight:"50px"}}><Link to="/contact-us"> Contact us</Link></li>
+            <li style={{display:"inline",paddingRight:"50px"}}><Button variant="contained" color="primary">Login</Button></li>
+
         </ul>
     </div> );
 }
