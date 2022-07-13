7909650f73b4eb0903bac8bfa5418ce72628829b
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2022-07-13 14:43:52 +0500

    makeStyle hook problem in mui v6
    -So we used style attribute as there is problem of makeStyles hook in mui v6

--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -1,24 +1,20 @@
-// import { AppBar, makeStyles, Toolbar, Typography, } from '@mui/material';
+import { AppBar, Toolbar, Typography, } from '@mui/material';
 
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
+/* import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles'; */
 import React from 'react';
 import { Link } from 'react-router-dom';
-var useStyles = makeStyles({
-    Link:{
-        color:"white",
-    }
-  });
+
 
 
 const TopMenu = () => {
-    var hookClass = useStyles();
+    // var hookClass = useStyles();
     return (
   <div>
         {<AppBar>
             <Toolbar>
                 <Typography variant="h6">
-                    <Link className={hookClass.Link} to="/">Home</Link>
+                    <Link to="/" style={{color:"white"}}>Home</Link>
                 </Typography>
             </Toolbar>
         </AppBar>}
